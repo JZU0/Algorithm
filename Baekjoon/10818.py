359f28abb60419ee97167283eb0ff5d1cc5b52df
JZU0 <ljzh1030@gmail.com>
2022-06-25 22:48:02 +0900

    1783

--- Baekjoon/10818.py
+++ Baekjoon/10818.py
@@ -2,4 +2,12 @@ n = int(input())
 list = list(map(int, input().split())) 
  
 print(min(list),end=" ") 
-print(max(list))
+print(max(list)) 
+ 
+'''  
+import sys
+
+n, *m = map(int, sys.stdin.read().split())
+
+print(min(m), max(m))
+'''
