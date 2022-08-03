dcca2ca33cccbb4348085f27f943ae8489dcdb6d
JZU0 <ljzh1030@gmail.com>
2022-08-03 14:22:39 +0900

    15649

---
+++ Baekjoon/15649.py
@@ -0,0 +1,14 @@
+import sys  
+import itertools
+ 
+N, M = map(int, sys.stdin.readline().split())   
+ 
+arr = [] 
+
+for i in range(1,N+1):
+    arr.append(str(i))   
+
+print("\n".join(list(map(" ".join,itertools.permutations(arr,M))))) 
+
+#for i in itertools.permutations(arr,M):
+#    print(*list(i))
